72dc0ffd5cd0da3557087fc948262be0928e9884
mac <berno579@gmail.com>
2022-09-19 23:38:48 +0900

    init commit

---
+++ src/questionBank/Problem17.java
@@ -0,0 +1,8 @@
+public class Problem17 {
+	public static void main(String[] args) {
+		int a, b, c, hap;
+		a = b = c = 2;
+		hap = ++a | b-- & c--;
+		System.out.printf("%d %d %d %d", hap, a, b, c);
+	}
+}
---
+++ src/section154/Ex2.java
@@ -0,0 +1,33 @@
+class ClassA {
+	ClassA() {
+		System.out.print('A');
+		this.prn();
+	}
+
+	void prn() {
+		System.out.print('B');
+	}
+}
+
+class ClassB extends ClassA {
+	ClassB() {
+		super();
+		System.out.print('D');
+	}
+
+	void prn() {
+		System.out.print('E');
+	}
+
+	void prn(int x) {
+		System.out.print(x);
+	}
+}
+
+public class Ex2 {
+	public static void main(String[] args) {
+		int x = 7;
+		ClassB cal = new ClassB();
+		cal.prn(x);
+	}
+}
